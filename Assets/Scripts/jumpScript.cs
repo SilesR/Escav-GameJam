d373f6c30a695d33924c032867a3c0ad037a973b
SilesR <rafasiles@gmail.com>
2014-12-06 01:56:10 +0100

    #04_Script_Salto[PERSONAJE]

---
+++ Assets/Scripts/jumpScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class jumpScript : MonoBehaviour {
+
+	public int jumpForce=200;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown ("Jump")) //Cuando pulsa espacio salta
+			saltar ();//mas abajo esta la funcion explicada
+	
+	}
+	void saltar(){
+
+		rigidbody2D.AddForce (new Vector2 (0, jumpForce));
+		}
+}
