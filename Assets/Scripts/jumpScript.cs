78d697eed4c1c37dcd2e039389c3f856f49cd82b
Rafael <rafasiles@gmail.com>
2014-12-06 20:57:54 +0100

    #24_Implementar_Animacion_Muerte

--- Assets/Scripts/jumpScript.cs
+++ Assets/Scripts/jumpScript.cs
@@ -3,18 +3,23 @@ using System.Collections;
 
 public class jumpScript : MonoBehaviour {
 
+
+
 	public int jumpForce=200;
 	public bool standing;
-
+	private Animator animator;
 
 
 	// Use this for initialization
 	void Start () {
-	
+		animator = GetComponent<Animator> ();
 	}
+
 	
 	// Update is called once per frame
 	void Update () {
+
+
 
 		var absVelY = Mathf.Abs(rigidbody2D.velocity.y);
 		if(absVelY <= .05f){
@@ -30,5 +35,12 @@ public class jumpScript : MonoBehaviour {
 	void saltar(){
 
 		rigidbody2D.AddForce (new Vector2 (0, jumpForce));
+	}
+	void OnCollisionEnter2D(Collision2D col){
+				if (col.gameObject.tag == "Enemigo") {
+						animator.SetBool ("muerto", true);
+						GameControl.dead = true;
+
+				}
 		}
 }
