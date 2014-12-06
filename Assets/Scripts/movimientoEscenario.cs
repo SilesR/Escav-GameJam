dac7e8cbc084307c9088549f66a0b41be6f3d6d5
Rafael <rafasiles@gmail.com>
2014-12-06 10:29:29 +0100

    #03_Script_Movimiento[ESCENARIO]

---
+++ Assets/Scripts/movimientoEscenario.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movimientoEscenario : MonoBehaviour {
+
+	public int vhorizontal=-3;
+
+	Vector3 movimiento;
+
+	
+	// Update is called once per frame
+	void Update () {
+			movimiento = new Vector3 (vhorizontal, 0, 0);
+		transform.Translate (movimiento * Time.deltaTime);
+	}
+}
