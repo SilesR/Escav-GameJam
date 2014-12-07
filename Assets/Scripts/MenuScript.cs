166a8781b3e4e178084cf81a97a5b5a263281e95
guillermolopezros <guillermolopezros@gmail.com>
2014-12-07 02:30:45 +0100

    #21_pantalla menu_Script menu

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+	public string nivelcarga;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GameControl.dead) {
+
+		}
+		
+	}
+	
+	
+	public  void start(){
+		GameControl.dead = false;
+		GameControl.score = 0;
+
+		Application.LoadLevel(nivelcarga);
+	}
+	
+	public  void quit(){
+		
+		Application.Quit();
+	}
+}
