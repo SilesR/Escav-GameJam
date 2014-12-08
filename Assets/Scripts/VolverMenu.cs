86e55b049f4c44123f15dbbf9b783e3b3a0624ea
SilesR <rafasiles@gmail.com>
2014-12-08 21:16:51 +0100

    #Script_Introduccion

---
+++ Assets/Scripts/VolverMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VolverMenu : MonoBehaviour {
+
+	public string menuse;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void volverMenu(){
+
+		Application.LoadLevel (menuse);
+	}
+
+}
