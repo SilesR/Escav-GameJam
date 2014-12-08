41e664a7a48b16d3af2a573d2ca7794f8ebae848
SilesR <rafasiles@gmail.com>
2014-12-08 21:08:53 +0100

    #Menu_Extras

---
+++ Assets/Scripts/Extras.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Extras : MonoBehaviour {
+
+	public string varios;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+
+	public void extras(){
+
+		Application.LoadLevel (varios);
+
+
+		}
+}
---
+++ Assets/Scripts/ExtrasVolver.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExtrasVolver : MonoBehaviour {
+	public string vuelve;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void volver(){
+		
+				Application.LoadLevel (vuelve);
+		}
+}
